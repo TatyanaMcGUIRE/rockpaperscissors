839227eec969458fe9468f6143c75b445e0b753f
Tatyana McGuire <tatyanam953@gmail.com>
2018-11-05 23:18:33 +0000

    rock paper scissors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-// *************************** YOUR CODE BELOW *******************************
+ // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
 /* global $ */
-
+$("button").click(function(){
+    $("#userChoice").text($("input").val());
+    
+});
 
 
 // DOCUMENT READY FUNCTION BELOW
