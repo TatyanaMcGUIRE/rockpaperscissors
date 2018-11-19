3a4cf0e3edac78531e6f91f0b965b465c40977cb
Tatyana McGuire <tatyanam953@gmail.com>
2018-11-19 23:19:22 +0000

    Unit 2 Session 2 work

--- js/script.js
+++ js/script.js
@@ -4,9 +4,25 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice="";
+var computerChoice="";
+var winner="";
+var randomNumber=0;
 $("button").click(function(){
-    $("#userChoice").text($("input").val());
     
+    userChoice= $("input").val()
+$("#userChoice").text($("input").val());
+randomNumber= Math.random()
+if(randomNumber<.33){
+    computerChoice= "rock"
+}
+else if (randomNumber<.66){
+    computerChoice= "paper"
+}
+else{
+    computerChoice= "scissors"
+}
+$("#computerChoice").text(computerChoice)
 });
 
 
